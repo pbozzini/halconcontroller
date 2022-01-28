8f36ab2ff96755d0ad978fec26ebb0711f1fd7d7
pbozzini <65220079+pbozzini@users.noreply.github.com>
2022-01-28 12:36:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'halconcontroller',         # How you named your package folder (MyLib)
   packages = ['halconcontroller'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This package allows to control MvTec HALCON Programs into Python.',   # Give a short description about your library
   author = 'Pascal Bozzini',                   # Type in your name
@@ -11,8 +11,8 @@ setup(
   download_url = 'https://github.com/pbozzini/halconcontroller.git',    # I explain this later on
   keywords = ['Halcon', 'Python', 'MvTec'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'halcon',
-          'os',
+          
+          
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
